66d57f3becfc828a91b84876bd885398cb64f7be
clovisdanielcosta <clovisdanielcosta@gmail.com>
2020-11-29 04:48:09 -0300

    Updated folder assets/img to assets/images to keep Angular's standard on GitHub Pages

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -15,7 +15,7 @@ export class CourseListComponent implements OnInit {
             {
                 id: 1,
                 name: 'Angular: Forms',
-                imageUrl: './assets/img/forms.png',
+                imageUrl: './assets/images/forms.png',
                 price: 99.99,
                 code: 'XPS-8796',
                 duration: 120,
@@ -26,7 +26,7 @@ export class CourseListComponent implements OnInit {
             {
                 id: 2,
                 name: 'Angular: Http',
-                imageUrl: './assets/img/http.png',
+                imageUrl: './assets/images/http.png',
                 price: 45.99,
                 code: 'LKL-1094',
                 duration: 80,
